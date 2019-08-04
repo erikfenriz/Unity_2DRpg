222786dd38fb3da44338562003235ffc0fae7385
Ernestovich <erikfenriz@gmail.com>
2019-08-04 12:19:31 +0300

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    private Animator animator;
+
+    protected Vector2 direction;
+
+    //private Rigidbody2D myRigidBody;
+
+    
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        //myRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        //myRigidBody.velocity = direction * speed;
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            AnimateMovement(direction);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+
+
+    }
+
+
+    public void AnimateMovement(Vector2 direction)
+    {
+
+        animator.SetLayerWeight(1, 1);
+
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    [SerializeField]
+    private Stats health;
+
+    [SerializeField]
+    private Stats mana;
+
+    [SerializeField]
+    private float initialHealth;
+
+    [SerializeField]
+    private float initialMana;
+
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+
+        health.Initialize(initialHealth, initialHealth);
+        mana.Initialize(initialMana, initialMana);
+
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+
+		base.Update();
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            mana.MyCurrentValue -= 10;
+            health.MyCurrentValue -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            mana.MyCurrentValue += 10;
+            health.MyCurrentValue += 10;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+}
